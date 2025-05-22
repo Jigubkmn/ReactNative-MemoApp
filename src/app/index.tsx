b246572eab5cd432fa0292c46d3c5c78b572dbfd
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-23 01:48:54 +0900

    ボタンスタイリング

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -25,7 +25,7 @@ export default function Page() {
       {/* メモリスト2 */}
       <View style={styles.memoListItem}>
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト1</Text>
+          <Text style={styles.memoListItemTitle}>買い物リスト2</Text>
           <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
         </View>
         <View>
@@ -36,7 +36,7 @@ export default function Page() {
       {/* メモリスト3 */}
       <View style={styles.memoListItem}>
         <View>
-          <Text style={styles.memoListItemTitle}>買い物リスト1</Text>
+          <Text style={styles.memoListItemTitle}>買い物リスト3</Text>
           <Text style={styles.memoListItemDate}>2023年10月1日 10:00</Text>
         </View>
         <View>
@@ -45,8 +45,8 @@ export default function Page() {
       </View>
 
 
-      <View>
-        <Text>+</Text>
+      <View style={styles.circleButton}>
+        <Text style={styles.circleButtonLabel}>+</Text>
       </View>
     </View>
   );
@@ -97,4 +97,27 @@ const styles = StyleSheet.create({
     lineHeight: 18,
     color: "#848484"
   },
+  circleButton: {
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    backgroundColor: "#467FD3",
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
+    right: 40,
+    bottom: 40,
+    // これはiosのみしか適用できない
+    shadowColor: "#000000",
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 8 },
+    // Android用のシャドウデザイン
+    elevation: 8,
+  },
+  circleButtonLabel: {
+    color: "#ffffff",
+    fontSize: 40,
+    lineHeight: 48
+  }
 });
