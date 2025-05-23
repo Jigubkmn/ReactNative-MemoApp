e6f98f371c8612b38f792469b99496123c1e3f0b
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-23 21:38:31 +0900

    ヘッダーをコンポーネント化対応

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,10 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Header } from "../components/Header";
 
 export default function Page() {
   return (
     <View style={styles.container}>
-      {/* ヘッダー */}
-      <View style={styles.header}>
-        <View style={styles.headerInner}>
-          <Text style={styles.headerTitle}>Memo App</Text>
-          <Text style={styles.headerRight}>ログアウト</Text>
-        </View>
-      </View>
-
+      <Header />
       {/* メモリスト1 */}
       <View style={styles.memoListItem}>
         <View>
@@ -44,7 +38,7 @@ export default function Page() {
         </View>
       </View>
 
-
+      {/* サークルボタン */}
       <View style={styles.circleButton}>
         <Text style={styles.circleButtonLabel}>+</Text>
       </View>
@@ -56,27 +50,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#ffffff",
-  },
-  header: {
-    backgroundColor: "#467FD3",
-    height: 104,
-    justifyContent: "flex-end",
-  },
-  headerInner: {
-    alignItems: "center",
-  },
-  headerTitle: {
-    marginBottom: 8,
-    fontSize: 22,
-    lineHeight: 32,
-    fontWeight: "bold",
-    color: "#ffffff"
-  },
-  headerRight: {
-    position: "absolute",
-    right: 16,
-    bottom: 16,
-    color: "rgba(255, 255,255, 0.7)"
   },
   memoListItem: {
     backgroundColor: "#f0f0f0",
