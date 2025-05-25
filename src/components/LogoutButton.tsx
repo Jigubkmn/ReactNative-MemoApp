c88a68a85a0eaa943fe2b0c97e0a5548ee81f47e
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-25 16:11:17 +0900

    ログアウトボタンの配置を修正

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -2,7 +2,7 @@ import { TouchableOpacity, Text, StyleSheet } from "react-native";
 
 export function LogoutButton () {
   return(
-    <TouchableOpacity>
+    <TouchableOpacity style={styles.container}>
       <Text style={styles.text}>ログアウト</Text>
     </TouchableOpacity>
 
@@ -10,6 +10,9 @@ export function LogoutButton () {
 }
 
 const styles = StyleSheet.create({
+  container: {
+    marginRight: 8,
+  },
   text: {
     fontSize: 12,
     lineHeight: 24,
