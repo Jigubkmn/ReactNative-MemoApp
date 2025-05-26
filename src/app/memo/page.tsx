6b824d3c572a72a5a436085302564a383d6df737
Masashi Shiomi <bkmn.31519@gmail.com>
2025-05-27 01:53:28 +0900

    ログインユーザーのデータを取得

--- src/app/memo/page.tsx
+++ src/app/memo/page.tsx
@@ -5,6 +5,9 @@ import { Feather } from "@expo/vector-icons";
 import { router, useNavigation } from "expo-router";
 import { useEffect } from "react";
 import { LogoutButton } from "../../components/LogoutButton";
+import { collection, onSnapshot, query, orderBy } from "firebase/firestore"
+import { db, auth } from "../../config"
+
 
 const handlePress = () => {
   router.push("/memo/create/page")
@@ -21,6 +24,22 @@ export default function Page() {
         }
       })
   }, [])
+
+  useEffect(() => {
+    if (auth.currentUser === null) { return }
+    // users/${auth.currentUser.uid}/memos：ログインユーザーのメモ一覧
+    const ref = collection(db, `users/${auth.currentUser.uid}/memos`)
+    // ほしいデータを選択
+    const q = query(ref, orderBy("updateAt", "desc"))
+    // メモのデータを監視
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      snapshot.forEach((doc) => {
+        console.log("memo", doc.data())
+      })
+    })
+    // 監視を削除
+    return unsubscribe
+  }, )
 
   return (
     <View style={styles.container}>
